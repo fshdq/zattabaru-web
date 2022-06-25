6ac3d3fc268e432e781fae0b85822050fba33b76
fshdq <m.farras@rocketmail.com>
2022-06-26 00:10:18 +0700

    dummy program

---
+++ assets/js/data_program.js
@@ -0,0 +1,42 @@
+const program = [
+  {
+    id: 1,
+    name: "Program 1",
+    description: "Program 1 description",
+    keunggulan: [
+      {
+        id: 1,
+        name: "Keunggulan 1",
+      },
+      {
+        id: 2,
+        name: "Keunggulan 2",
+      },
+      {
+        id: 3,
+        name: "Keunggulan 3",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Program 2",
+    description: "Program 2 description",
+    keunggulan: [
+      {
+        id: 1,
+        name: "Keunggulan 1",
+      },
+      {
+        id: 2,
+        name: "Keunggulan 2",
+      },
+      {
+        id: 3,
+        name: "Keunggulan 3",
+      },
+    ],
+  },
+];
+
+export default program;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 import '../css/style.css'
 import Alpine from 'alpinejs'
+import program from './data_program'
  
 window.Alpine = Alpine
  
 Alpine.start()
+Alpine.data('data_program', program)
