94a00db4c93418d05f4c1ad1b6ac9675a7cf6428
fshdq <m.farras@rocketmail.com>
2022-06-28 08:42:26 +0700

    alasan memilih kami bug

---
+++ assets/js/data_alasan.js
@@ -0,0 +1,34 @@
+const alasan = [
+  {
+    id: 1,
+    name: "Berijin Resmi",
+    description: "Zatabbaru telah memiliki ijin resmi dan telah memberangkatkan lebih dari ratusan jamaah.",
+  },
+  {
+    id: 2,
+    name: "Full service tanpa ribet urus dokumen",
+    description: "Zatabbaru memiliki layanan penuh untuk memudahkan calon jamaah dalam pengurusan dokumen.",
+  },
+  {
+    id: 3,
+    name: "Berbagai Program Unggulan",
+    description: "Zatabbaru memiliki berbagai program unggulan yang tidak ditemukan di biro lain",
+  },
+  {
+    id: 4,
+    name: "Ikatan Alumni Solid",
+    description: "zatabbaru memiliki ikatan alumni yang solid, dengan masih terjalinnya selaturahmi bahkan setelah kegiatan umroh selesai",
+  },
+  {
+    id: 5,
+    name: "Terbimbing, ibadah maksimal",
+    description: "Zatabbaru mengedepankan kualitas, sehingga menjamin seluruh jamaah terbimbing demi mencapai ibadah yang maksimal",
+  },
+  {
+    id: 6,
+    name: "Manasik Sepuasnya",
+    description: "Zatabbaru memiliki fasilitas manasik tanpa batas dan tambahan biaya. Sehingga bagi jamaah yang belum paham berkesempatan mengulang sampai paham ",
+  }
+];
+
+export default alasan;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 import '../css/style.css'
 import Alpine from 'alpinejs'
 import program from './data_program'
+import alasan from './data_alasan'
  
 window.Alpine = Alpine
  
-Alpine.start()
 Alpine.data('data_program', program)
+Alpine.data('data_alasan', alasan)
+Alpine.start()
