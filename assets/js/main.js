9280e5e40a069abd57cf3c1becc48e155d07d6c5
fshdq <m.farras@rocketmail.com>
2022-07-09 23:16:41 +0700

    add alasan, add testimonial, install swiper js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 import '../css/style.css'
+import 'swiper/css';
 import Alpine from 'alpinejs'
 import program from './data_program'
 import alasan from './data_alasan'
+import Swiper from 'swiper';
+
+const swiper = new Swiper();
  
 window.Alpine = Alpine
  
